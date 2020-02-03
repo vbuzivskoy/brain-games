07fd1c0e2683c5d7d75aff4318ff34cc182b3e1c
Buzivskoy Vyacheslav <vyacheslav.buzivskoy@ukmig.com.ua>
2020-02-03 10:58:40 +0200

    Add 'src\index.js' with askNameAndSayHello function and use it at brain-games.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+
+const askNameAndSayHello = () => {
+    console.log("Welcome to the Brain Games!\n");
+    const name = readlineSync.question('Your answer: ');
+    console.log(`Hello, ${name}`);
+};
+
+export default askNameAndSayHello;
