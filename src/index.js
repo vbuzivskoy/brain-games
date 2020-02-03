52e815ff61e5daa2d33f6e93e8d4ff2cb514cb4d
vbuzivskoy <v.buzivskoy@gmail.com>
2020-02-03 16:47:09 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const askNameAndSayHello = () => {
-  console.log("Welcome to the Brain Games!\n");
+  console.log('Welcome to the Brain Games!\n');
   const name = readlineSync.question('Your answer: ');
   console.log(`Hello, ${name}`);
 };
