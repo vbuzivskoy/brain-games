d32c9ebdce8d52ee079f30e2edea29684b451540
Buzivskoy Vyacheslav <vyacheslav.buzivskoy@ukmig.com.ua>
2020-02-06 16:10:21 +0200

    Main code refactoring: add gameplayLoop

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,28 +1,6 @@
 #!/usr/bin/env node
 
-import { sayGreetings, askName, sayHello } from '../src/index.js';
-import * as evenGame from '../src/evenGame.js';
+import { gameplayLoop } from '../src/index.js';
+import * as evenGame from '../src/games/evenGame.js';
 
-sayGreetings();
-evenGame.showRules();
-console.log('');
-const playerName = askName();
-sayHello(playerName);
-console.log('');
-
-const answersToWin = 3;
-let correctAnswers = 0;
-let isGameLost = false;
-while (correctAnswers < answersToWin && !isGameLost) {
-  if (evenGame.askIsEven()) {
-    correctAnswers += 1;
-  } else {
-    isGameLost = true;
-  }
-}
-
-if (isGameLost) {
-  evenGame.showTryAgain(playerName);
-} else {
-  evenGame.showCongrats(playerName);
-}
+gameplayLoop(evenGame.gameplay, evenGame.rules);
--- src/index.js
+++ src/index.js
@@ -4,13 +4,41 @@ const sayGreetings = () => {
   console.log('Welcome to the Brain Games!');
 };
 
-const askName = () => {
-  const name = readlineSync.question('May I have your name? ');
-  return name;
-};
+const askName = () => readlineSync.question('May I have your name? ');
 
 const sayHello = (name) => {
-  console.log(`Hello, ${name}`);
+  console.log(`Hello, ${name}\n`);
 };
 
-export { sayGreetings, askName, sayHello };
+const gameplayLoop = (gameplayFunction, gameRules) => {
+  sayGreetings();
+  console.log(`${gameRules}\n`);
+  const playerName = askName();
+  sayHello(playerName);
+
+  const answersToWin = 3;
+  let correctAnswers = 0;
+  let isGameLost = false;
+  while (correctAnswers < answersToWin && !isGameLost) {
+    const answers = gameplayFunction();
+    const playerAnswer = answers.playerAns;
+    const correctAnswer = answers.correctAns;
+    if (playerAnswer === correctAnswer) {
+      correctAnswers += 1;
+      console.log('Correct!');
+    } else {
+      console.log(`'${playerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      isGameLost = true;
+    }
+  }
+
+  if (isGameLost) {
+    console.log(`Let's try again, ${playerName}!`);
+  } else {
+    console.log(`Congratulations, ${playerName}!`);
+  }
+};
+
+export {
+  gameplayLoop, sayGreetings, sayHello, askName,
+};
